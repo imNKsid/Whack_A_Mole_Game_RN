bd9c162642ce7f63e34ec2f1664f0bda53dd77e6
Nitesh Kumar <imnksid@gmail.com>
2024-03-19 16:42:27 +0530

    Built initial setup

---
+++ ios/Pods/Target Support Files/Pods-Whack_A_Mole-Whack_A_MoleTests/Pods-Whack_A_Mole-Whack_A_MoleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Whack_A_Mole_Whack_A_MoleTests : NSObject
+@end
+@implementation PodsDummy_Pods_Whack_A_Mole_Whack_A_MoleTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-Whack_A_Mole/Pods-Whack_A_Mole-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Whack_A_Mole : NSObject
+@end
+@implementation PodsDummy_Pods_Whack_A_Mole
+@end
